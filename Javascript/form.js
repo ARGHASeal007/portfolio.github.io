38c735bf89321ee994a69f4cb16c5e5ac03578b8
Argha Seal <sealargha@gmail.com>
2024-01-30 00:34:53 +0530

    fixed the form

--- Javascript/form.js
+++ Javascript/form.js
@@ -13,18 +13,34 @@
 //     );
 // }
 
-export function gotowhatsapp() {
-    
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
 
-    console.log(name,email,message);
 
-    var url = "https://wa.me/917003989928?text=" 
-    + "Name: " + name + "%0a"
-    + "E-mail: " + email  + "%0a"
-    + "Message: " + message; 
+// export function gotowhatsapp() {
+//     console.log('form sumbited');
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("message").value;
 
-    window.open(url, '_blank').focus();
-}
+//     console.log(name,email,message);
+
+//     var url = "https://wa.me/917003989928?text=" 
+//     + "Name: " + name + "%0a"
+//     + "E-mail: " + email  + "%0a"
+//     + "Message: " + message; 
+
+//     window.open(url, '_blank').focus();
+// }
+
+
+    let form = document.getElementById('contact-form');  // let, const, var difference // callback function and arrow function difference
+    form.addEventListener('submit', (e)=>{              //what is event while form submission?
+            e.preventDefault();
+            let formData = new FormData(form);
+            let data = Object.fromEntries(formData.entries());
+            var url = "https://wa.me/917003989928?text=" 
+                + "Name: " + data.name + "%0a"
+                + "E-mail: " + data.email  + "%0a"
+                + "Message: " + data.message; 
+            
+                window.open(url, '_blank').focus();
+    })
